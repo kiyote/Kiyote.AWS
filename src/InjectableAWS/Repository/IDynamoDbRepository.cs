72adf2dc81188ca64cc466eaa07a84aea950f245
Todd Lang <todd.lang@d2l.com>
2022-01-21 13:52:14 -0500

    Extracting DynamoDbRepository interface.

---
+++ src/InjectableAWS/Repository/IDynamoDbRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace InjectableAWS.Repository;
+
+public interface IDynamoDbRepository {
+	Task CreateAsync<TData>(
+		TData data,
+		CancellationToken cancellationToken
+	);
+
+	Task<TData?> LoadAsync<TData>(
+		string pk,
+		string sk,
+		CancellationToken cancellationToken
+	);
+
+	Task<IEnumerable<TData>> QueryPrefixAsync<TData>(
+		string pk,
+		IEnumerable<string> prefixes,
+		CancellationToken cancellationToken
+	);
+
+	Task<IEnumerable<TData>> QueryPrefixAsync<TData>(
+		string pk,
+		string prefix,
+		CancellationToken cancellationToken
+	);
+}
